638d1913ef8854d5a58d41ade3c5f2ab346515fb
BrunnoFardindeSouza <136929628+BrunnoFardindeSouza@users.noreply.github.com>
2023-06-30 06:39:10 -0300

    .

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 // MENU FLUTUANTE
-let jornais = document.getElementById('jornais')
 let gaveta = document.getElementById('gaveta-apps')
 let itensmenu = document.getElementById('itensMenu')
 let previous = document.getElementById('previous')
@@ -10,22 +9,20 @@ gaveta.addEventListener('click',()=>{
     }else{
         itensmenu.style.display='flex'
     }
-
+    
 })
 previous.addEventListener('click',()=>{
     itensmenu.style.display='none'
-
+    
 })
+
 // FORMULÁRIO FLUTUANTE
-let btn_entrar = document.getElementById('entraruser')
+
 let login = document.getElementById("login")
 let formVoltar = document.getElementById('formVoltar')
+let btn_entrar = document.getElementById('entraruser')
 let formulario = document.getElementById('formulario')
 
-let JornalVoltar = document.getElementById('JornalVoltar')
-let acessoPainel = document.getElementById('AcessoPainel')
-let painelAdm = document.getElementById('painelAdm')
-let PainelAdm = document.getElementById('PainelAdm')
 
 login.addEventListener('click',()=>{
     formulario.style.display='block'
@@ -34,9 +31,9 @@ login.addEventListener('click',()=>{
 
 formVoltar.addEventListener('click',()=>{
     formulario.style.display='none'
-
     
-
+    
+    
 })
 btn_entrar.addEventListener('click',()=>{
     var coleta_user = document.getElementById('usuário')
@@ -50,30 +47,42 @@ btn_entrar.addEventListener('click',()=>{
     }
 })
 
-// CRIAÇÃO DE JORNAIS
+// PAINEL ADM
 
-c = 1
-while(c<=6){
-    var criarDiv = document.createElement('div')
-    var criarSpan = document.createElement('span') 
-    criarSpan.setAttribute('class','material-symbols-outlined')
-    criarSpan.innerText=' download'
-    criarDiv.appendChild(criarSpan)
-    jornais.appendChild(criarDiv)
-    c++
-
-}
-// painel adm
-
-
+let acessoPainel = document.getElementById('AcessoPainel')
+let painelAdm = document.getElementById('painelAdm')
+let PainelAdm = document.getElementById('PainelAdm')
 
 acessoPainel.addEventListener('click',()=>{
     itensmenu.style.display='none'
     PainelAdm.style.display='block'
-
-
-
+    
+    
+    
 })
+
+// CRIAÇÃO DE JORNAIS
+
+let jornais = document.getElementById('jornais')
+let JornalVoltar = document.getElementById('JornalVoltar')
+let qtd_jronais = document.getElementById('publicacao')
+let btn_jornal = document.getElementById("btn_jornal")
+
 JornalVoltar.addEventListener('click',()=>{
     PainelAdm.style.display='none'
+})
+c = 1
+
+btn_jornal.addEventListener('click',()=>{
+    let qtd_jronal = Number(qtd_jronais.value)
+    while(c<=qtd_jronal){
+        var criarDiv = document.createElement('div')
+        var criarSpan = document.createElement('span') 
+        criarSpan.setAttribute('class','material-symbols-outlined')
+        criarSpan.innerText=' download'
+        criarDiv.appendChild(criarSpan)
+        jornais.appendChild(criarDiv)
+        c++
+    
+    }
 })
